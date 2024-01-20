de01dbf288ede8f5a180e78b25b316a300c30cff
coleccionciclista <92733304+coleccionciclista@users.noreply.github.com>
2024-01-21 00:40:26 +0100

    ,

--- js/botonariba.js
+++ js/botonariba.js
@@ -1,18 +1,18 @@
-  // Mostrar el botón cuando se haya desplazado cierta cantidad desde la parte superior
-  window.onscroll = function() {
-    showScrollButton();
-  };
+// Mostrar el botón cuando se haya desplazado cierta cantidad desde la parte superior
+window.onscroll = function () {
+  showScrollButton();
+};
 
-  function showScrollButton() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      document.getElementById("Siguiente").style.display = "block";
-    } else {
-      document.getElementById("Siguiente").style.display = "none";
-    }
+function showScrollButton() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("Siguiente").style.display = "block";
+  } else {
+    document.getElementById("Siguiente").style.display = "none";
   }
+}
 
-  // Función para desplazarse a la parte superior de la página
-  function scrollToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  }
+// Función para desplazarse a la parte superior de la página
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
